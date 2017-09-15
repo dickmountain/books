0021b9afdcd6d15a763d756ef2d33c68e500cdcf
Max <maxatr@yandex.ru>
2017-09-15 14:34:44 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, result){
+    
+});
+
+app.listen(process.env.PORT, process.env.HOST, function(){
+    
+});
