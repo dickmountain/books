054e045e0e0a9c342fb01503f3d6a7272159bd38
Max <maxatr@yandex.ru>
2017-09-17 17:52:26 +0000

    Adding book routes

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
+
+var books = [
+        {author:'Владимир Познер', image:'https://ozon-st.cdn.ngenix.net/multimedia/1012171211.jpg'},
+        {author:'Richard Dawkins', image:'https://ozon-st.cdn.ngenix.net/multimedia/1012171212.jpg'},
+        {author:'Владимир Познер', image:'https://ozon-st.cdn.ngenix.net/multimedia/1012171213.jpg'}
+];
 
 app.get('/', function(request, result){
     result.render('landing');
@@ -12,13 +20,19 @@ app.get('/', function(request, result){
 });
 
 app.get('/books', function(request, result){
-    var books = [
-        {author:'Владимир Познер', image:'https://ozon-st.cdn.ngenix.net/multimedia/1012171211.jpg'},
-        {author:'Richard Dawkins', image:'https://ozon-st.cdn.ngenix.net/multimedia/1012171212.jpg'},
-        {author:'Владимир Познер', image:'https://ozon-st.cdn.ngenix.net/multimedia/1012171213.jpg'}
-    ];
-    
     result.render('books', {books:books});
+});
+
+app.get('/books/new', function(request, result){
+    result.render('new');
+});
+
+app.post('/books', function(request, result){
+    var author = request.body.author;
+    var image = request.body.image;
+    books.push({author:author, image:image});
+    
+    result.redirect('/books');
 });
 
 app.listen(process.env.PORT, process.env.HOST, function(){
