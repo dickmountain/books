120dd434f00649df4d12e7256cf376de79b43493
Max <maxatr@yandex.ru>
2017-09-27 21:12:33 +0000

    Show page added

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ mongoose.connect('mongodb://localhost/books', {
 });
 var bookSchema = new mongoose.Schema({
     author:String,
-    image:String
+    image:String,
+    description:String
 });
 var Book = mongoose.model('Book', bookSchema);
 
@@ -21,14 +22,10 @@ app.get('/', function(request, result){
     result.render('landing');
 });
 
-app.get('/', function(request, result){
-    result.render('landing');
-});
-
 app.get('/books', function(request, result){
     Book.find({}, function(err, books){
         if(!err){
-            result.render('books', {books:books});
+            result.render('index', {books:books});
         }
     });
 });
@@ -37,18 +34,30 @@ app.get('/books/new', function(request, result){
     result.render('new');
 });
 
+app.get('/books/:id', function(request, result){
+    Book.findById(request.params.id, function(err, book){
+        if(!err){
+            result.render('show', {book:book});
+        }
+    });
+});
+
 app.post('/books', function(request, result){
     var author = request.body.author;
     var image = request.body.image;
+    var description = request.body.description;
+    var title = request.body.title;
    
-   Book.create({
-       author:author,
-       image:image
-   }, function(err, book){
-       if(!err){
-           result.redirect('/books');
-       }
-   });
+    Book.create({
+        author:author,
+        image:image,
+        description:description,
+        title:title
+    }, function(err, book){
+        if(!err){
+            result.redirect('/books');
+        }
+    });
 });
 
 app.listen(process.env.PORT, process.env.HOST, function(){
